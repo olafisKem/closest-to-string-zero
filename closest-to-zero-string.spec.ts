0756384bf0661a27c99d3ff87fd1073af18057cf
olafisKem <folafis@googlemail.com>
2024-11-22 14:43:16 +0000

    feat: empty strings pass

--- closest-to-zero-string.spec.ts
+++ closest-to-zero-string.spec.ts
@@ -2,8 +2,8 @@ import { describe, expect, it } from "@jest/globals";
 import { closestToZeroString } from "./closest-to-zero-string";
 
 describe ('closestToZeroString', () => {
-    it('should return null if no value is given', () => {
-        expect(closestToZeroString(' ')).toEqual("Hello, World!");
+    it('should return an empty string', () => {
+        expect(closestToZeroString(' ')).toEqual(' ');
     });
 
 });
