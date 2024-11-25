36320581e4e0cabb1ca1f6670a055262b83a56ab
olafisKem <folafis@googlemail.com>
2024-11-25 10:32:50 +0000

    feat: return the one string in the array

--- closest-to-zero-string.spec.ts
+++ closest-to-zero-string.spec.ts
@@ -3,9 +3,10 @@ import { closestToZeroString } from "./closest-to-zero-string";
 
 describe ('closestToZeroString', () => {
     it('should return an empty string', () => {
-        expect(closestToZeroString([])).toEqual([]);
+        expect(closestToZeroString([])).toEqual("");
     });
-    it('should return the string if the array has one string', () => {
-        expect(closestToZeroString([0])).toEqual([0]);
+    it('should return the one string in the array', () => {
+        expect(closestToZeroString(['Zero'])).toBe('Zero');
+        expect(closestToZeroString(['One'])).toBe('One');
     });
 });
--- closest-to-zero-string.ts
+++ closest-to-zero-string.ts
@@ -1,1 +1,8 @@
-export const closestToZeroString = (xz: string[]) => [];
+export const closestToZeroString = (xs: string[]) => {
+    if (xs.length === 0 || xs[0] === "") {
+        return "";
+    }
+    if (xs.length === 1) {
+        return xs[0];
+    }
+};
